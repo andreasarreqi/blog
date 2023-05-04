e127cd6263f11ec6f62cbee731e01ce79e86adbd
andreasarreqi <>
2023-05-04 19:59:34 +0000

    Creating the superuser and the Musician/PostAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Musician, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Musician)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('artist', 'genre', 'slug', 'status', 'created_on')
+    search_fields = ['artist', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('artist', )}
+    summernote_fields = ('content', )
