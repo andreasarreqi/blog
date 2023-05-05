8b934dc440954fcd24a10b7849c8b493cf87a1ba
andreasarreqi <>
2023-05-05 15:02:08 +0000

    Creating templates folder and static folder

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Musician
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Musician
+    queryset = Musician.objects.filter(status=1).order.by("-created_on")
+    temnplate_name = 'index.html'
+    paginate_by = 8
