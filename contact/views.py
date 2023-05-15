d781d416776bc38fec643766160855473830620e
andreasarreqi <>
2023-05-15 00:13:59 +0000

    Completing the contactus view thus making the contact page form functional, sending data to the database

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic, View
 from .forms import ContactForm
 from .models import Contact
@@ -11,12 +11,26 @@ class ContactUs(View):
 
     model = Contact
     template_name = 'contact.html'
+    form_class = ContactForm
     success_url = '/'
 
     def get(self, request):
         """
-        Gets information through the contact form
+        Gets the actual form to display on the page.
         """
 
         context = {'form': ContactForm()}
+        return render(request, 'contact.html', context)
+
+    def post(self, request):
+        """
+        This function actually posts the data, sends the data to the database.
+        """
+        if request.method == "POST":
+            form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            form = ContactForm()
         return render(request, 'contact.html', context)
